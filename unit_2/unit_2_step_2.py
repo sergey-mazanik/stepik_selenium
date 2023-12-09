4505d51733b4e4c125c6603a4e0f326332b22dc6
Sergey Mazanik <sergey.mazanik1@gmail.com>
2023-12-09 23:45:33 +0300

    add unit 2 and empty file for one of the tasks

--- unit_2/unit_2_step_2.py
+++ unit_2/unit_2_step_2.py
@@ -21,3 +21,85 @@
 # finally:
 #     time.sleep(10)
 #     browser.quit()
+
+
+# from selenium import webdriver
+# import time
+# browser = webdriver.Chrome()
+# browser.execute_script("alert('Robots at work');")
+# time.sleep(3)
+
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+#
+# browser = webdriver.Chrome()
+# link = "https://SunInJuly.github.io/execute_script.html"
+# browser.get(link)
+# button = browser.find_element(By.TAG_NAME, "button")
+# browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+# button.click()
+
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# import time
+# import math
+#
+#
+# def calc(x):
+#     return str(math.log(abs(12 * math.sin(int(x)))))
+#
+# try:
+#     link = "https://SunInJuly.github.io/execute_script.html"
+#     browser = webdriver.Chrome()
+#     browser.get(link)
+#     x_element = browser.find_element(By.CSS_SELECTOR, 'span[id="input_value"]')
+#     x = x_element.text
+#     y = calc(x)
+#     field = browser.find_element(By.ID, 'answer')
+#     browser.execute_script('return arguments[0].scrollIntoView(true);', field)
+#     field.send_keys(y)
+#     input2 = browser.find_element(By.CSS_SELECTOR, 'input[id="robotCheckbox"]')
+#     input2.click()
+#     time.sleep(1)
+#     input3 = browser.find_element(By.CSS_SELECTOR, 'input[id="robotsRule"]')
+#     input3.click()
+#     time.sleep(1)
+#     input4 = browser.find_element(By.CSS_SELECTOR, 'button.btn')
+#     input4.click()
+#     time.sleep(1)
+#
+# finally:
+#     # ожидание чтобы визуально оценить результаты прохождения скрипта
+#     time.sleep(10)
+#     # закрываем браузер после всех манипуляций
+#     browser.quit()
+
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# import time
+# import os
+#
+# link = 'http://suninjuly.github.io/file_input.html'
+# browser = webdriver.Chrome()
+# current_dir = os.path.abspath(os.path.dirname(__file__))
+# file_path = os.path.join(current_dir, 'file.txt')
+#
+# try:
+#     browser.get(link)
+#     input1 = browser.find_element(By.NAME, 'firstname')
+#     input1.send_keys('firstname')
+#     input2 = browser.find_element(By.NAME, 'lastname')
+#     input2.send_keys('lastname')
+#     input3 = browser.find_element(By.NAME, 'email')
+#     input3.send_keys('test@test.com')
+#     input4 = browser.find_element(By.ID, 'file')
+#     input4.send_keys(file_path)
+#     input5 = browser.find_element(By.CSS_SELECTOR, 'button.btn')
+#     input5.click()
+#
+# finally:
+#     time.sleep(10)
+#     browser.quit()
